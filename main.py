c1db1484c2298b305566fcba9109e7a93d261af3
Lucas Souza <lucas_souza@outlook.com>
2022-10-31 22:17:35 -0300

    add files

---
+++ main.py
@@ -0,0 +1,49 @@
+# from pprintpp import pprint as pp
+from db.database import Graph
+from helper.write_a_json import write_a_json as wj
+
+db = Graph(uri='BOA SORTE', user='E', password='ÓTIMA PROVA! 😉')
+
+# Questão 01
+# A
+
+# wj(aux, '1A')
+
+# B
+
+#wj(aux, '1B')
+
+# C
+
+#wj(aux, '1C')
+
+# D
+
+#wj(aux, '1D')
+
+# Questão 02
+# A
+
+#wj(aux, '2A')
+
+# B
+
+#wj(aux, '2B')
+
+# C
+
+#wj(aux, '2C')
+
+# D
+
+#wj(aux, '2D')
+
+# Questão 03
+# A
+
+# B
+
+# C
+
+# D
+
